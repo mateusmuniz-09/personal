d1afda1a06ee8999a784603da1664d3398a795ad
Mateus Muniz <mateus09muniz@gmail.com>
2025-03-09 17:35:26 -0300

    adicionando-form

---
+++ index.js
@@ -0,0 +1,17 @@
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+    const formObject = Object.fromEntries(formData);
+
+    const response = await fetch("http://localhost:3000/send", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formObject),
+    });
+
+    const result = await response.json();
+    alert(result.message);
+  });
